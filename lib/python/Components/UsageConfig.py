6b487fe40ecebb402063603552b91666088eeddf
javilonas <admin@lonasdigital.com>
2013-11-09 04:23:47 +0100

    UsageConfig: Increase timeout infobar to 6 seconds

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -52,7 +52,7 @@ def InitUsageConfig():
 	choicelist = []
 	for i in range(1, 11):
 		choicelist.append(("%d" % i, ngettext("%d second", "%d seconds", i) % i))
-	config.usage.infobar_timeout = ConfigSelection(default = "5", choices = [("0", _("No timeout"))] + choicelist)
+	config.usage.infobar_timeout = ConfigSelection(default = "6", choices = [("0", _("No timeout"))] + choicelist)
 	config.usage.show_infobar_on_zap = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_skip = ConfigYesNo(default = True)
 	config.usage.show_infobar_on_event_change = ConfigYesNo(default = False)
@@ -60,7 +60,7 @@ def InitUsageConfig():
 	config.usage.show_infobar_lite = ConfigYesNo(default = False)
 	config.usage.show_infobar_channel_number = ConfigYesNo(default = False)
 	config.usage.show_second_infobar = ConfigSelection(default = "1", choices = [("0", _("Off")), ("1", _("Event Info")), ("2", _("2nd Infobar INFO")), ("3", _("2nd Infobar ECM"))])
-	config.usage.second_infobar_timeout = ConfigSelection(default = "5", choices = [("0", _("No timeout"))] + choicelist)
+	config.usage.second_infobar_timeout = ConfigSelection(default = "6", choices = [("0", _("No timeout"))] + choicelist)
 	def showsecondinfobarChanged(configElement):
 		if config.usage.show_second_infobar.getValue() != "INFOBAREPG":
 			SystemInfo["InfoBarEpg"] = True
