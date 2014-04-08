571ec156c9c19185d976f65e7f1c3b489bdf3965
javilonas <admin@lonasdigital.com>
2014-04-08 22:45:03 +0200

    [BluePanel] Created shortcut menu

--- lib/python/Plugins/Extensions/LDteam/plugin.py
+++ lib/python/Plugins/Extensions/LDteam/plugin.py
@@ -1,8 +1,21 @@
 from Plugins.Plugin import PluginDescriptor
-
+from Screens.PluginBrowser import *
+	
+def LDpanel(menuid, **kwargs):
+	if menuid == "mainmenu":
+		return [("Blue Panel", main, "LDBluePanel", 11)]
+	else:
+		return []
+		
 def main(session, **kwargs):
 	from LdBlue import LDBluePanel
 	session.open(LDBluePanel)
-
+		
 def Plugins(**kwargs):
-	return PluginDescriptor(name = "LDteam", description = "Lets you execute your LDPanels", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc = main)
+	return [
+
+	#// show panel in Main Menu
+	PluginDescriptor(name="Blue Panel", description="Blue panel - OpenLD", where = PluginDescriptor.WHERE_MENU, fnc = LDpanel),
+	#// show panel in EXTENSIONS Menu
+	PluginDescriptor(name="Blue Panel", description="Blue panel - OpenLD", where = PluginDescriptor.WHERE_EXTENSIONSMENU, fnc = main) ]
+
