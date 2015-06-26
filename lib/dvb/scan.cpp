OpenLD/enigma2

commit db5e288d99d9c14a1cfb545efe17779c2ba2800e

eDVBScan: fix NULL pointer segfault

lib/dvb/scan.cpp
@@ -150,8 +150,7 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
 				ePtr<iDVBFrontend> fe;
-				ePtr<eDVBFrontend> frontend;
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && frontend->supportsDeliverySystem(SYS_DVBT2, true))
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
 				{
 					/* we have to scan T2 as well as T */
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;

commit 965993ef61b372f6919a694f097f9d3631310900

eDVBScan: fix compile error
introduced by db5e288
use iDVBFrontend::isCompatible instead of eDVBFrontend::supportsDeliverySystem

lib/dvb/scan.cpp
@@ -149,14 +149,23 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 			{
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
-				ePtr<iDVBFrontend> fe;
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2)
 				{
 					/* we have to scan T2 as well as T */
+					ePtr<iDVBFrontend> fe;
+					eDVBFrontendParameters eparm;
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;
-					ePtr<eDVBFrontendParameters> feparm = new eDVBFrontendParameters;
-					feparm->setDVBT(parm);
-					addChannelToScan(feparm);
+					eparm.setDVBT(parm);
+					m_channel->getFrontend(fe);
+					if (fe)
+					{
+						ePtr<iDVBFrontendParameters> feparm = new eDVBFrontendParameters(eparm);
+						/* but only if the frontend supports T2 */
+						if (fe->isCompatibleWith(feparm))
+						{
+							addChannelToScan(feparm);
+						}
+					}
 				}
 			}
 		}