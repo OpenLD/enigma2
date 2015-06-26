OpenLD/enigma2

commit 0d77be001512940cd438bef57c4b8e01593ddd19

scan.cpp: perform T2 and T scan only,
when frontend is actually supporting DVB-T2. This will also fix USB DVB-T full service scan and tunein issues with -1 entries in lamedb.

Signed-off-by: littlesat <littlesat99@yahoo.com>

lib/dvb/scan.cpp
@@ -140,7 +140,7 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 		m_channel_state = state;
 	} else if (state == iDVBChannel::state_failed)
 	{
-		if (m_ch_current)
+		if (m_ch_current && m_channel)
 		{
 			int type;
 			m_ch_current->getSystem(type);
@@ -149,7 +149,9 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 			{
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2)
+				ePtr<iDVBFrontend> fe;
+				ePtr<eDVBFrontend> frontend;
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && frontend->supportsDeliverySystem(SYS_DVBT2, true))
 				{
 					/* we have to scan T2 as well as T */
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;

commit 842a51f25b201389448f7e43d9f2c9b0f884c309

eDVBScan: fix T->T2 fallback
broken by 0d77be0

Instead of having a fallback when 'system' was
not specified in terrestrial.xml, we were doing a
fallback when scanning T2 transponders instead.

lib/dvb/scan.cpp
@@ -150,7 +150,7 @@ void eDVBScan::stateChange(iDVBChannel *ch)
 				eDVBFrontendParametersTerrestrial parm;
 				m_ch_current->getDVBT(parm);
 				ePtr<iDVBFrontend> fe;
-				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
+				if (parm.system == eDVBFrontendParametersTerrestrial::System_DVB_T_T2 && !m_channel->getFrontend(fe) && fe && fe->supportsDeliverySystem(SYS_DVBT2, true))
 				{
 					/* we have to scan T2 as well as T */
 					parm.system = eDVBFrontendParametersTerrestrial::System_DVB_T2;