228aa2618dfc7418e598d28b76b34355e9ce24c1
Captain <captain.onboard@web.de>
2015-08-10 20:46:00 +0200

    fix fegasus build

--- lib/python/Tools/Profile.py
+++ lib/python/Tools/Profile.py
@@ -57,7 +57,7 @@ def profile(id):
 				elif boxtype in ("mixosf5", "gi9196m", "osmini"):
 					f = open("/proc/progress", "w")
 					f.write("%d" % perc)
-				elif boxtype in ("xpeedlx3", "sezammarvel", "atemionemesis", "fegasusx5"):
+				elif boxtype in ("xpeedlx3", "sezammarvel", "atemionemesis", "fegasusx3", "fegasusx5s", "fegasusx5t"):
 					f = open("/proc/vfd", "w")
 					f.write("Loading %d %%" % perc)
 				elif boxtype in ("azboxhd", "azboxme"):
