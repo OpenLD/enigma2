5bbdae5fe82f98ccf634c52abe711ba8c853928f
Javier Sayago <admin@lonasdigital.com>
2015-09-05 19:32:16 +0200

    Fix EPG

--- lib/dvb/epgcache.h
+++ lib/dvb/epgcache.h
@@ -42,8 +42,8 @@
 
 #ifdef ENABLE_MHW_EPG
 
-#define FILE_EQUIV "/usr/lib/enigma2/python/Plugins/Extensions/LDteam/epg/mhw_Equiv.epg"
-#define FILE_CHANNELS "/usr/lib/enigma2/python/Plugins/Extensions/LDteam/epg/mhw_Chann.epg"
+#define FILE_EQUIV "/etc/mhw_Equiv.epg"
+#define FILE_CHANNELS "/etc/mhw_Chann.epg"
 #define FILE_LOG "/tmp/mhw_Log.epg"
 
 #define EPG_REPLAY_LEN 8
