c449996d337b2b6034b724cabeef30e333001951
javilonas <admin@lonasdigital.com>
2015-12-11 21:26:33 +0100

    Revert "Fix conflict timers"
    This reverts commit ad0755e3b8bf8aea86978df9df18ae24276b2cd1.

--- lib/python/Components/TimerSanityCheck.py
+++ lib/python/Components/TimerSanityCheck.py
@@ -186,8 +186,9 @@ class TimerSanityCheck:
 					fakeRecResult = 0
 				if not fakeRecResult: # tune okay
 					if hasattr(fakeRecService, 'frontendInfo') and hasattr(fakeRecService.frontendInfo(), 'getFrontendData'):
-						feinfo = fakeRecService.frontendInfo().getFrontendData()
-						tunerType.append(feinfo.get("tuner_type"))
+					feinfo = fakeRecService.frontendInfo()
+					if feinfo:
+						tunerType.append(feinfo.getFrontendData().get("tuner_type"))
 				else: # tune failed.. so we must go another way to get service type (DVB-S, DVB-T, DVB-C)
 
 					def getServiceType(ref): # helper function to get a service type of a service reference
