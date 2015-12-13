60b3f44479fcd1dc9138d4a113ee91f3c69d96db
Javier Sayago <admin@lonasdigital.com>
2015-12-13 23:08:47 +0100

    Test Change EPG data 2

--- lib/python/Plugins/Extensions/LDteam/LdSet.py
+++ lib/python/Plugins/Extensions/LDteam/LdSet.py
@@ -48,7 +48,7 @@ config.plugins.LDteam.dropmode = ConfigSelection(default = '1', choices = [
 		('3', _("free pagecache, dentries and inodes")),
 		])
 config.plugins.LDteam.epgtime2 = ConfigClock(default = ((16*60) + 15) * 60)
-config.plugins.LDteam.epgmhw2wait = ConfigNumber(default = 490 ) # 490 seconds = 8,16 minutes
+config.plugins.LDteam.epgmhw2wait = ConfigNumber(default = 350 ) # 490 seconds = 5,83 minutes
 
 def mountp():
 	pathmp = []
