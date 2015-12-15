0f8fcfb6e8116a721b1dda874cfec0006da66d22
javilonas <admin@lonasdigital.com>
2015-12-15 01:06:40 +0100

    Revert "Menu eventinfo/extension: manual re-order list"
    This reverts commit 2796ad93818c60c3826617f87ca5b76341cbaf09.

--- lib/python/Screens/ChoiceBox.py
+++ lib/python/Screens/ChoiceBox.py
@@ -1,18 +1,12 @@
 from Screens.Screen import Screen
-from Screens.MessageBox import MessageBox
 from Components.ActionMap import NumberActionMap
-from Components.config import config, ConfigSubsection, ConfigText
 from Components.Label import Label
 from Components.ChoiceList import ChoiceEntryComponent, ChoiceList
 from Components.Sources.StaticText import StaticText
 import enigma
 
-config.misc.pluginlist = ConfigSubsection()
-config.misc.pluginlist.eventinfo_order = ConfigText(default="")
-config.misc.pluginlist.extension_order = ConfigText(default="")
-
 class ChoiceBox(Screen):
-	def __init__(self, session, title="", list=None, keys=None, selection=0, skin_name=None, reorderConfig="", text=""):
+	def __init__(self, session, title="", list=None, keys=None, selection=0, skin_name=None, text=""):
 		self.setTitle(_("Choice Box"))
 		if not list: list = []
 		if not skin_name: skin_name = []
@@ -21,7 +15,6 @@ class ChoiceBox(Screen):
 		if isinstance(skin_name, str):
 			skin_name = [skin_name]
 		self.skinName = skin_name + ["ChoiceBox"]
-		self.reorderConfig = reorderConfig
 		self["text"] = Label()
 		if title:
 			title = _(title)
@@ -55,16 +48,6 @@ class ChoiceBox(Screen):
 
 		self.keymap = {}
 		pos = 0
-		if self.reorderConfig:
-			prev_list = list
-			new_list = []
-			self.config_type = eval("config.misc.pluginlist." + self.reorderConfig)
-			for x in self.config_type.value.split(","):
-				for entry in prev_list:
-					if entry[0] == x:
-						new_list.append(entry)
-						prev_list.remove(entry)
-			list = new_list + prev_list
 		for x in list:
 			strpos = str(self.__keys[pos])
 			self.list.append(ChoiceEntryComponent(key = strpos, text = x))
@@ -77,7 +60,7 @@ class ChoiceBox(Screen):
 		self["summary_selection"] = StaticText()
 		self.updateSummary(selection)
 
-		self["actions"] = NumberActionMap(["WizardActions", "InputActions", "ColorActions", "DirectionActions", "MenuActions"],
+		self["actions"] = NumberActionMap(["WizardActions", "InputActions", "ColorActions"],
 		{
 			"ok": self.go,
 			"1": self.keyNumberGlobal,
@@ -96,9 +79,6 @@ class ChoiceBox(Screen):
 			"blue": self.keyBlue,
 			"up": self.up,
 			"down": self.down,
-			"moveUp": self.additionalMoveUp,
-			"moveDown": self.additionalMoveDown,
-			"menu": self.setDefaultChoiceList
 			"left": self.left,
 			"right": self.right
 		}, -1)
@@ -234,42 +214,3 @@ class ChoiceBox(Screen):
 
 	def cancel(self):
 		self.close(None)
-
-	def setDefaultChoiceList(self):
-		if self.reorderConfig:
-			if len(self.list) > 0 and self.config_type.value != "":
-				self.session.openWithCallback(self.setDefaultChoiceListCallback, MessageBox, _("Sort list to default and exit?"), MessageBox.TYPE_YESNO)
-		else:
-			self.cancel()
-
-	def setDefaultChoiceListCallback(self, answer):
-		if answer:
-			self.config_type.value = ""
-			self.config_type.save()
-			self.cancel()
-
-	def additionalMoveUp(self):
-		if self.reorderConfig:
-			self.additionalMove(-1)
-
-	def additionalMoveDown(self):
-		if self.reorderConfig:
-			self.additionalMove(1)
-
-	def additionalMove(self, direction):
-		if len(self.list) > 1:
-			currentIndex = self["list"].getSelectionIndex()
-			swapIndex = (currentIndex + direction) % len(self.list)
-			if currentIndex == 0 and swapIndex != 1:
-				self.list = self.list[1:] + [self.list[0]]
-			elif swapIndex == 0 and currentIndex != 1:
-				self.list = [self.list[-1]] + self.list[:-1]
-			else:
-				self.list[currentIndex], self.list[swapIndex] = self.list[swapIndex], self.list[currentIndex]
-			self["list"].l.setList(self.list)
-			if direction == 1:
-				self["list"].down()
-			else:
-				self["list"].up()
-			self.config_type.value = ",".join(x[0][0] for x in self.list)
-			self.config_type.save()
