d58bd7393e202655b1c6ea4d0a7c3befca23bcb4
Javier Sayago <admin@lonasdigital.com>
2016-01-17 16:01:58 +0100

    Fix videomode

--- lib/python/Screens/VideoMode.py
+++ lib/python/Screens/VideoMode.py
@@ -133,6 +133,7 @@ class VideoSetup(Screen, ConfigListScreen):
 		self["config"].l.setList(self.list)
 		if config.usage.sort_settings.value:
 			self["config"].list.sort()
+		return
 
 	def keyLeft(self):
 		ConfigListScreen.keyLeft(self)
@@ -667,9 +668,9 @@ class AutoVideoMode(Screen):
 		iAVSwitch.setWss(config.av.wss)
 		iAVSwitch.setPolicy43(config.av.policy_43)
 		iAVSwitch.setPolicy169(config.av.policy_169)
-
 		self.delay = False
 		self.detecttimer.stop()
+		return
 
 def autostart(session):
 	global resolutionlabel
@@ -681,3 +682,4 @@ def autostart(session):
 		config.av.autores.setValue(False)
 		config.av.autores.save()
 		configfile.save()
+	return
