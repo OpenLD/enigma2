1592469e3c23e9b70171c632b17ac0fe1dda1fd8
captain <openatv@gmail.com>
2016-03-15 10:32:02 +0100

    fix build issue dvb api < 5 multistream

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -3000,7 +3000,11 @@ eDVBRegisteredFrontend *eDVBFrontend::getLast(eDVBRegisteredFrontend *fe)
 
 bool eDVBFrontend::is_multistream()
 {
+#if DVB_API_VERSION >= 5
 	return fe_info.caps & FE_CAN_MULTISTREAM;
+#else //if DVB_API_VERSION < 5
+	return fe_info.caps & false;
+#endif
 }
 
 std::string eDVBFrontend::getCapabilities()
