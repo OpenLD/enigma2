7d40ac745d68d5ee8208df0ea3eaac95e8876b6b
Javier Sayago <admin@lonasdigital.com>
2016-05-21 17:10:12 +0200

    SNR approximation in dB for Sundtek SkyTV Ultimate 5 (DVB-S/S2) tuner

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1083,6 +1083,11 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 		ret = (int)(snr / 75);
 		ter_max = 1700;
 	}
+	else if (strstr(m_description, "Sundtek DVB-S/S2 (IV)"))
+	{
+		ret = (int)(snr / 40.5);
+		sat_max = 1900;
+	}
 	else if(!strcmp(m_description, "TBS-5925") || !strcmp(m_description, "DVBS2BOX"))
 	{
 		ret = (snr * 2000) / 0xFFFF;
@@ -1176,7 +1181,7 @@ int eDVBFrontend::readFrontendData(int type)
 				}
 #endif
 				// fallback to old DVB API
-				if(!signalquality && !signalqualitydb || strstr(m_description, "Sundtek DVB-T (III)"))
+				if(!signalquality && !signalqualitydb || strstr(m_description, "Sundtek"))
 				{
 					int snr = readFrontendData(iFrontendInformation_ENUMS::snrValue);
 					calculateSignalQuality(snr, signalquality, signalqualitydb);
