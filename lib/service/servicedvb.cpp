e2980d6fc206d8695dd29a36af542165b6b12123
Javier Sayago <admin@lonasdigital.com>
2016-07-05 22:59:20 +0200

    enigma2: Fix-problems-because-of-disabled-debugging-messages

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1546,7 +1546,10 @@ RESULT eDVBServicePlay::setFastForward_internal(int ratio, bool final_seek)
 	m_skipmode = skipmode;
 
 	if (final_seek)
-		eDebug("trickplay stopped .. ret %d, pos %lld", getPlayPosition(pos), pos);
+	{
+		RESULT r = getPlayPosition(pos);
+		eDebug("[eDVBServicePlay] setFastForward trickplay stopped .. ret %d, pos %lld", r, pos);
+	}
 
 	m_fastforward = ffratio;
 
@@ -1561,7 +1564,10 @@ RESULT eDVBServicePlay::setFastForward_internal(int ratio, bool final_seek)
 		ret = m_decoder->setTrickmode();
 
 	if (pos)
-		eDebug("final seek after trickplay ret %d", seekTo(pos));
+	{
+		RESULT r = seekTo(pos);
+		eDebug("[eDVBServicePlay] setFastForward final seek after trickplay ret %d", r);
+	}
 
 	return ret;
 }
@@ -2125,6 +2131,7 @@ int eDVBServicePlay::selectAudioStream(int i)
 	eDVBServicePMTHandler::program program;
 	eDVBServicePMTHandler &h = m_timeshift_active ? m_service_handler_timeshift : m_service_handler;
 	pts_t position = -1;
+	RESULT ret;
 
 	if (h.getProgramInfo(program))
 		return -1;
@@ -2148,18 +2155,24 @@ int eDVBServicePlay::selectAudioStream(int i)
 	}
 
 	if (i != -1 && apid != m_current_audio_pid && (m_is_pvr || m_timeshift_active))
-		eDebug("getPlayPosition ret %d, pos %lld in selectAudioStream", getPlayPosition(position), position);
+	{
+		ret = getPlayPosition(position);
+		eDebug("[eDVBServicePlay] getPlayPosition ret %d, pos %lld in selectAudioStream", ret, position);
+	}
 
 	m_current_audio_pid = apid;
 
 	if (m_decoder->setAudioPID(apid, apidtype))
 	{
-		eDebug("set audio pid failed");
+		eDebug("[eDVBServicePlay] set audio pid %04x failed", apid);
 		return -4;
 	}
 
 	if (position != -1)
-		eDebug("seekTo ret %d", seekTo(position));
+	{
+		ret = seekTo(position);
+		eDebug("[eDVBServicePlay] seekTo ret %d", ret);
+	}
 
 	int rdsPid = apid;
 
