e8e557fc896c0eb371f42c386b4503dd9be12444
captain <openatv@gmail.com>
2016-08-07 19:48:18 +0200

    [aspectSelection] fix green long selection

--- lib/python/Components/AVSwitch.py
+++ lib/python/Components/AVSwitch.py
@@ -354,7 +354,7 @@ class AVSwitch:
 		return aspect[0] * fb_size.height(), aspect[1] * fb_size.width()
 
 	def setAspectRatio(self, value):
-		pass
+		eAVSwitch.getInstance().setAspectRatio(value)
 
 	def getAspectRatioSetting(self):
 		valstr = config.av.aspectratio.value
