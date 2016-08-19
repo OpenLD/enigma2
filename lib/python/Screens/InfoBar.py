85dc44d4e7a2c274b52f915adf932dfd4ca7220e
Javier Sayago <admin@lonasdigital.com>
2016-08-19 12:51:20 +0200

    InfoBar: Add sanity check to avoid possible GSOD

--- lib/python/Screens/InfoBar.py
+++ lib/python/Screens/InfoBar.py
@@ -542,7 +542,7 @@ class MoviePlayer(InfoBarBase, InfoBarShowHide, InfoBarLongKeyDetection, InfoBar
 		if not config.movielist.stop_service.value:
 			Screens.InfoBar.InfoBar.instance.callServiceStarted()
 		self.session.nav.playService(self.lastservice)
-		config.usage.last_movie_played.value = self.cur_service.toString()
+		config.usage.last_movie_played.value = self.cur_service and self.cur_service.toString() or ""
 		config.usage.last_movie_played.save()
 
 	def handleLeave(self, how):
