503e46e06f609b4a126a0156de662981dc401603
littlesat <littlesat99@yahoo.com>
2016-08-22 19:13:35 +0200

    Only stop service in satconfig when this is really required

--- lib/python/Screens/Satconfig.py
+++ lib/python/Screens/Satconfig.py
@@ -561,6 +561,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 		return checkRecursiveConnect(self.slotid)
 
 	def keyOk(self):
+		self.stopService()
 		if self["config"].getCurrent() == self.advancedSelectSatsEntry and self.advancedSelectSatsEntry:
 			conf = self.nimConfig.advanced.sat[int(self.nimConfig.advanced.sats.value)].userSatellitesList
 			self.session.openWithCallback(boundFunction(self.updateConfUserSatellitesList, conf), SelectSatsEntryScreen, userSatlist=conf.value)
@@ -623,14 +624,13 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 				self.deleteConfirmed(confirmed)
 			break
 		else:
-			self.restoreService(_("Zap back to service before tuner setup?"))
+			self.restartPrevService()
 
 	def __init__(self, session, slotid):
 		Screen.__init__(self, session)
 		Screen.setTitle(self, _("Tuner settings"))
 		self.list = [ ]
 		ServiceStopScreen.__init__(self)
-		self.stopService()
 		ConfigListScreen.__init__(self, self.list)
 
 		self["key_red"] = Label(_("Close"))
@@ -696,7 +696,7 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 		if self["config"].isChanged():
 			self.session.openWithCallback(self.cancelConfirm, MessageBox, _("Really close without saving settings?"), default = False)
 		else:
-			self.restoreService(_("Zap back to service before tuner setup?"))
+			self.restartPrevService()
 
 	def saveAll(self):
 		if self.nim.isCompatible("DVB-S"):
@@ -716,12 +716,11 @@ class NimSetup(Screen, ConfigListScreen, ServiceStopScreen):
 	def cancelConfirm(self, result):
 		if not result:
 			return
-
 		for x in self["config"].list:
 			x[1].cancel()
 		# we need to call saveAll to reset the connectedTo choices
 		self.saveAll()
-		self.restoreService(_("Zap back to service before tuner setup?"))
+		self.restartPrevService()
 
 	def changeConfigurationMode(self):
 		if self.configMode:
