64a0abf8cf6857081ce596f6a69b80533821e693
Javier Sayago <admin@lonasdigital.com>
2016-10-18 19:06:31 +0200

    Update StreamingClientsInfo

--- lib/python/Screens/StreamingClientsInfo.py
+++ lib/python/Screens/StreamingClientsInfo.py
@@ -1,30 +1,44 @@
 from Screen import Screen
 from Components.ActionMap import ActionMap
+from Components.Button import Button
+from Components.ScrollLabel import ScrollLabel
 from Components.Converter.ClientsStreaming import ClientsStreaming
+from Components.config import config
+from Components.Sources.StaticText import StaticText
 import skin
 import gettext
-from Components.Sources.StaticText import StaticText
+
 
 class StreamingClientsInfo(Screen):
-		skin ="""<screen name="StreamingClientsInfo" position="0,0" size="1280,720" flags="wfNoBorder" backgroundColor="#90000000">
-	<eLabel position="center,117" zPosition="-2" size="600,500" backgroundColor="layer-a-background" />
-	<widget source="Title" render="Label" position="center,126" size="580,44" font="Regular; 35" transparent="1" valign="top" zPosition="0" backgroundColor="layer-b-background" halign="center" />
-	<widget source="total" render="Label" position="center,174" size="580,50" zPosition="1" font="Regular; 22" halign="left" backgroundColor="layer-a-background" transparent="1" valign="center" />
-	<widget source="liste" render="Label" position="center,234" size="580,370" zPosition="1" transparent="1" noWrap="1" font="Regular; 20" valign="top" />
+	skin ="""<screen name="StreamingClientsInfo" position="center,center" size="720,630" flags="wfNoBorder">
+	<eLabel position="center,27" zPosition="-2" size="680,600" backgroundColor="#25062748" />
+	<widget source="Title" render="Label" position="center,36" size="540,44" font="Regular;35" valign="top" zPosition="0" halign="center" />
+	<widget source="total" render="Label" position="center,104" size="540,50" zPosition="1" font="Regular; 22" halign="left" backgroundColor="#25062748" valign="center" />
+	<widget source="liste" render="Label" position="center,164" size="540,370" zPosition="1" noWrap="1" font="Regular;21" valign="top" />
+	<widget source="ScrollLabel" render="Label" position="0,00" size="540,470" zPosition="2" font="Regular;19" halign="left"/>
 </screen>"""
-		def __init__(self, session):
-			Screen.__init__(self, session)
-			self.setTitle(_("Streaming clients info"))
-			if ClientsStreaming("NUMBER").getText() == "0":
-				self["total"] = StaticText( _("No streaming Channel from this STB at this moment") )
-				text = ""
-			else:
-				self["total"] = StaticText( _("Total Clients streaming: ") + ClientsStreaming("NUMBER").getText())
-				text =  ClientsStreaming("EXTRA_INFO").getText()
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		self.setTitle(_("Streaming clients info"))
 
-			self["liste"] = StaticText(text)
-			self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
+		if ClientsStreaming("NUMBER").getText() == "0":
+			self["total"] = StaticText( _("No stream clients") )
+			text = ""
+		else:
+			self["total"] = StaticText( _("Total Clients streaming: ") + ClientsStreaming("NUMBER").getText())
+			text = ClientsStreaming("EXTRA_INFO").getText()
+			clients = ClientsStreaming("INFO_RESOLVE")
+			text = clients.getText()
+
+		self["liste"] = StaticText(text)
+		self["ScrollLabel"] = ScrollLabel(text)
+
+		self["key_red"] = Button(_("Close"))
+		self["actions"] = ActionMap(["ColorActions", "SetupActions", "DirectionActions"],
 			{
 				"cancel": self.close,
-				"ok": self.close
+				"ok": self.close,
+				"red": self.close,
+				"up": self["ScrollLabel"].pageUp,
+				"down": self["ScrollLabel"].pageDown
 			})
