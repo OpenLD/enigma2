1b21ddc657e068d2f818ae03c5c61ad2bc7a1e82
Athanasios Oikonomou <athoik@gmail.com>
2016-10-22 20:36:42 +0200

    multistream: use correct default values on PositionerSetup

--- lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/plugin.py
@@ -1462,7 +1462,7 @@ class TunerScreen(ConfigListScreen, Screen):
 		ConfigListScreen.keyRight(self)
 
 	def keyGo(self):
-		returnvalue = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+		returnvalue = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 1)
 		satpos = int(self.tuning.sat.value)
 		if self.tuning.type.value == "manual_transponder":
 			if self.scan_sat.system.value == eDVBFrontendParametersSatellite.System_DVB_S2:
