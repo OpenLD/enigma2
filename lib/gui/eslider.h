200f35500c49886fb19ada5e40a8af8ad27d7dfc
arn354 <arn354@email.de>
2016-11-30 22:42:15 +0100

    eslider - missed additions of previous commit

--- lib/gui/eslider.h
+++ lib/gui/eslider.h
@@ -36,11 +36,14 @@ private:
 		evtChangedSlider = evtUserWidget
 	};
 	bool m_have_border_color, m_have_foreground_color;
-	int m_min, m_max, m_value, m_start, m_orientation, m_orientation_swapped, m_border_width;
+	bool m_have_sliderborder_color, m_have_sliderforeground_color, m_have_sliderborder_width;
+	int m_min, m_max, m_value, m_start, m_orientation, m_orientation_swapped, m_border_width, m_sliderborder_width;
 	ePtr<gPixmap> m_pixmap, m_backgroundpixmap;
+	ePtr<gPixmap> m_scrollbarslidepixmap, m_scrollbarslidebackgroundpixmap;
 
 	gRegion m_currently_filled;
 	gRGB m_border_color, m_foreground_color;
+	gRGB m_sliderborder_color, m_sliderforeground_color;
 };
 
 #endif
