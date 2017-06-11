ed38bc7e23df821fe188b80425bcbaf586a2083c
Javier Sayago <admin@lonasdigital.com>
2017-06-11 17:26:26 +0200

    Update Pixmap

--- lib/python/Components/Pixmap.py
+++ lib/python/Components/Pixmap.py
@@ -12,11 +12,20 @@ class Pixmap(GUIComponent):
 
 	def getSize(self):
 		s = self.instance.size()
-		return s.width(), s.height()
+		return (s.width(), s.height())
+
+	def setPixmap(self, pixmap):
+		self.instance.setPixmap(pixmap)
+
+	def setShowHideAnimation(self, key):
+		self.instance.setShowHideAnimation(key)
+
+	def resetShowHideAnimation(self):
+		self.instance.setShowHideAnimation("")
 
 class PixmapConditional(ConditionalWidget, Pixmap):
 	def __init__(self, withTimer = True):
-		ConditionalWidget.__init__(self)
+		ConditionalWidget.__init__(self, withTimer)
 		Pixmap.__init__(self)
 
 class MovingPixmap(Pixmap):
@@ -35,7 +44,7 @@ class MovingPixmap(Pixmap):
 		self.moveTimer.callback.append(self.doMove)
 
 	def clearPath(self, repeated = False):
-		if self.moving:
+		if (self.moving):
 			self.moving = False
 			self.moveTimer.stop()
 
@@ -72,12 +81,12 @@ class MovingPixmap(Pixmap):
 		except: # moving not possible... widget not there any more... stop moving
 			self.stopMoving()
 
-		if self.time == 0:
+		if (self.time == 0):
 			self.currDest += 1
 			self.moveTimer.stop()
 			self.moving = False
-			if self.currDest >= len(self.path): # end of path
-				if self.repeated:
+			if (self.currDest >= len(self.path)): # end of path
+				if (self.repeated):
 					self.currDest = 0
 					self.moving = False
 					self.startMoving()
@@ -136,4 +145,4 @@ class MultiPixmap(Pixmap):
 			if len(self.pixmaps) > x:
 				self.instance.setPixmap(self.pixmaps[x])
 			else:
-				print "setPixmapNum(%d) failed! defined pixmaps:" % x, self.pixmaps
+				print "setPixmapNum(%d) failed! defined pixmaps:" %(x), self.pixmaps
