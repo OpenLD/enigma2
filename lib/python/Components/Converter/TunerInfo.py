c5c9b2e7354b0cf2401610d64a46933a2d05ab11
Javier Sayago <admin@lonasdigital.com>
2017-08-05 22:18:38 +0200

    update files

--- lib/python/Components/Converter/TunerInfo.py
+++ lib/python/Components/Converter/TunerInfo.py
@@ -5,28 +5,32 @@ from Components.Element import cached
 class TunerInfo(Converter, object):
 	def __init__(self, type):
 		Converter.__init__(self, type)
-		self.type = {
-				"TunerUseMask": TunerInfoSource.FE_USE_MASK
-			}[type]
+		self.type = {'TunerUseMask': TunerInfoSource.FE_USE_MASK,
+		 'TunerAvailable': TunerInfoSource.TUNER_AVAILABLE}[type]
 
 	@cached
 	def getBoolean(self):
 		if self.type == TunerInfoSource.FE_USE_MASK:
 			return self.source.getTunerUseMask() and True or False
+
 	boolean = property(getBoolean)
 
 	@cached
 	def getText(self):
 		if self.type == TunerInfoSource.FE_USE_MASK:
 			return str(self.source.getTunerUseMask())
-		return ""
+		return ''
+
 	text = property(getText)
 
 	@cached
 	def getValue(self):
 		if self.type == TunerInfoSource.FE_USE_MASK:
 			return self.source.getTunerUseMask()
+		if self.type == TunerInfoSource.TUNER_AVAILABLE:
+			return self.source.getTunerAmount()
 		return -1
+
 	value = property(getValue)
 
 	def changed(self, what):
