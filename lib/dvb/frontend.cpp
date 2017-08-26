094cd732485f55fe6f2527b839ea39997622b5d6
Athanasios Oikonomou <athoik@gmail.com>
2017-08-26 20:46:55 +0200

    Revert 'frontend: set some default min/max frequencies for simulated frontends'
    We are now initialize the fe_info directly from tuner for sumalated fronteds as well. Remove the dead code.

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -730,11 +730,6 @@ int eDVBFrontend::openFrontend()
 		m_sn = eSocketNotifier::create(eApp, m_fd, eSocketNotifier::Read, false);
 		CONNECT(m_sn->activated, eDVBFrontend::feEvent);
 	}
-	else
-	{
-		fe_info.frequency_min = 900000;
-		fe_info.frequency_max = 2200000;
-	}
 
 	m_multitype = (
 		m_delsys[SYS_DVBS] && m_delsys[SYS_DVBT])
