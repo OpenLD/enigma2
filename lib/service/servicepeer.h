6ce16229c9434215da7d0477a6fc45560959f817
Javier Sayago <admin@lonasdigital.com>
2017-10-04 20:51:48 +0200

    Fix servicepeer

--- lib/service/servicepeer.h
+++ lib/service/servicepeer.h
@@ -2,7 +2,7 @@
 #include <lib/python/python.h>
 
 #ifndef SWIG
-# include <string>
+#include <string>
 void init_servicepeer();
 void done_servicepeer();
 bool getAnyPeerStreamingBox(std::string &result);
