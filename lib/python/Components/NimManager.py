69d7e6703ee6cae2b5bbd07ad4b977a325c60502
Athanasios Oikonomou <athoik@gmail.com>
2018-04-23 12:08:18 +0200

    InitNimManager: check multitype detection at the end
    Move multitype detection to the bottom, it makes code simpler
    since we don't have to keep a list of multitype tuners require
    tunerTypeChange.
    
    The tunerTypeChange will be executed as that last step.

--- lib/python/Components/NimManager.py
+++ lib/python/Components/NimManager.py
@@ -2191,34 +2191,6 @@ def InitNimManager(nimmgr, update_slots = []):
 			except Exception as e:
 				print "[InitNimManager] tunerTypeChanged error: ", e
 
-	tunersRequireTypeChange = []
-
-	empty_slots = 0
-	for slot in nimmgr.nim_slots:
-		x = slot.slot
-
-		if update_slots and (x not in update_slots):
-			continue
-
-		nim = config.Nims[x]
-		addMultiType = False
-		try:
-			nim.multiType
-		except:
-			addMultiType = True
-		if slot.isMultiType() and addMultiType:
-			typeList = []
-			for id in slot.getMultiTypeList().keys():
-				type = slot.getMultiTypeList()[id]
-				typeList.append((id, type))
-			nim.multiType = ConfigSelection(typeList, "0")
-
-			nim.multiType.feid = x - empty_slots
-			nim.multiType.addNotifier(boundFunction(tunerTypeChanged, nimmgr), initial_call=False)
-			tunersRequireTypeChange.append(x)
-
-		print "[InitNimManager] slotname = %s, slotdescription = %s, multitype = %s, current type = %s" % (slot.input_name, slot.description, (slot.isMultiType() and addMultiType), slot.getType())
-
 	empty_slots = 0
 	for slot in nimmgr.nim_slots:
 		x = slot.slot
@@ -2301,8 +2273,20 @@ def InitNimManager(nimmgr, update_slots = []):
 		if empty:
 			empty_slots += 1
 
-	for x in tunersRequireTypeChange:
-		nim = config.Nims[x]
-		tunerTypeChanged(nimmgr, nim.multiType, initial=True)
+		# check for multitype tuners
+		addMultiType = False
+		try:
+			nim.multiType
+		except:
+			addMultiType = True
+
+		if slot.isMultiType() and addMultiType:
+			typeList = []
+			for id in slot.getMultiTypeList().keys():
+				type = slot.getMultiTypeList()[id]
+				typeList.append((id, type))
+			nim.multiType = ConfigSelection(typeList, "0")
+			nim.multiType.feid = x - empty_slots
+			nim.multiType.addNotifier(boundFunction(tunerTypeChanged, nimmgr), initial_call=True)
 
 nimmanager = NimManager()
