108e6c53659c6bc8737bf3d6204d1744a0a00a09
Javier Sayago <admin@lonasdigital.com>
2018-05-13 18:03:41 +0200

    Revert "update fb lcd grab to png"
    This reverts commit bc3b4715514c896f5185b9164a36df473c6fc4e4.

--- lib/gdi/fblcd.h
+++ lib/gdi/fblcd.h
@@ -25,7 +25,6 @@ class eFbLCD: public eLCD
 	uint16_t m_red[256], m_green[256], m_blue[256], m_trans[256];
 	int m_phys_mem;
 	int m_manual_blit;
-	bool m_dump;
 
 	void calcRamp();
 	int setMode(int xRes, int yRes, int bpp);
@@ -34,7 +33,6 @@ class eFbLCD: public eLCD
 	void disableManualBlit();
 			// low level gfx stuff
 	int putCMAP();
-	void dumpLCD2PNG();
 public:
 	eFbLCD(const char *fb=LCD_DEV);
 	~eFbLCD();
@@ -52,7 +50,7 @@ public:
 	int lock();
 	void unlock();
 	int islocked() { return locked; }
-	void setDump(bool);
+	void setDump(bool) {}
 };
 
 #endif
