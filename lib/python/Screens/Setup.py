aa7e9214ec1c84a67b27654c6f9f858d305b951a
iPkay <iPKay@gmx.com>
2018-12-28 20:37:43 +0100

    [Setup] keyword 'value' for comparing of non bool config  values added (for show or hiding setup entries)

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -235,6 +235,7 @@ class Setup(ConfigListScreen, Screen):
 					continue
 
 				requires = x.get("requires")
+				value = x.get("value")
 				if requires and requires.startswith('config.'):
 					item = eval(requires or "")
 					if item.value and not item.value == "0":
@@ -242,7 +243,7 @@ class Setup(ConfigListScreen, Screen):
 					else:
 						SystemInfo[requires] = False
 
-				if requires and not SystemInfo.get(requires, False):
+				if requires and not SystemInfo.get(requires, False) or value and not item.value==value:
 					continue
 
 				if self.PluginLanguageDomain:
