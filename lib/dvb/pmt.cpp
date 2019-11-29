4d923f0552351550a42fc35c7f67dcd93bab8d2c
javilonas <admin@lonasdigital.com>
2019-11-29 01:01:37 +0100

    Update files

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -322,6 +322,7 @@ void eDVBServicePMTHandler::AITready(int error)
 			{
 				std::string hbbtvUrl = "", applicationName = "";
 				std::string boundaryExtension = "";
+				std::string TPDescPath = "", SALDescPath = "";
 
 				int controlCode = (*i)->getApplicationControlCode();
 				ApplicationIdentifier * applicationIdentifier = (ApplicationIdentifier *)(*i)->getApplicationIdentifier();
@@ -386,7 +387,7 @@ void eDVBServicePMTHandler::AITready(int error)
 									InterActionTransportConstIterator interactionit = transport->getInteractionTransports()->begin();
 									for(; interactionit != transport->getInteractionTransports()->end(); ++interactionit)
 									{
-										hbbtvUrl = (*interactionit)->getUrlBase()->getUrl();
+										TPDescPath = (*interactionit)->getUrlBase()->getUrl();
 										break;
 									}
 									break;
@@ -399,7 +400,7 @@ void eDVBServicePMTHandler::AITready(int error)
 						case SIMPLE_APPLICATION_LOCATION_DESCRIPTOR:
 						{
 							SimpleApplicationLocationDescriptor *applicationlocation = (SimpleApplicationLocationDescriptor*)(*desc);
-							hbbtvUrl += applicationlocation->getInitialPath();
+							SALDescPath = applicationlocation->getInitialPath();
 							break;
 						}
 						case APPLICATION_USAGE_DESCRIPTOR:
@@ -408,6 +409,7 @@ void eDVBServicePMTHandler::AITready(int error)
 							break;
 						}
 					}
+					hbbtvUrl = TPDescPath + SALDescPath;
 				}
 				if(!hbbtvUrl.empty())
 				{
